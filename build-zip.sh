8d77b5fd99a8e32d6e27b382998a9d79047d73c3
Marwane Kalam-Alami <marwane.ka@gmail.com>
2019-03-10 17:07:32 +0100

    Remove custom ZIP, we can link to Github's one instead

--- build-zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Rebuilds the asset archive.
-# Tip: To make sure you don't forget updating this,
-# consider adding a "./build-zip.sh" command to your pre-commit Git hook.
-
-ARCHIVE_NAME=alakajam-presskit.zip
-
-rm -f $ARCHIVE_NAME
-zip -r $ARCHIVE_NAME fonts images sounds *.md
